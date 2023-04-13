e2dc87806aee66e5d716d46fc242fecafb87b257
Emmanuel Enwenede <emmanuel@blueeel.co.uk>
2023-04-13 10:42:07 +0100

    Hot reloading changes

--- apps/my-app/.storybook/main.ts
+++ apps/my-app/.storybook/main.ts
@@ -1,3 +1,5 @@
+import { dirname } from "path";
+
 import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -17,10 +19,11 @@ const config: StorybookConfig = {
   core: {
     builder: {
       name: "@storybook/builder-webpack5",
-      options: { }
+      options: {}
     }
   },
   webpackFinal: async (config) => {
+    console.log(dirname(require.resolve("react")));
     const fallback = {
       assert: require.resolve('assert'),
       path: require.resolve('path-browserify'),
@@ -33,9 +36,15 @@ const config: StorybookConfig = {
     };
     (config.resolve as any).alias = {
       ...config.resolve?.alias,
-      // To work with mdx stories
-      // We need to find a way to resolve react/jsx-runtime correctly.
-      react: require.resolve('react'),
+      // This is done this way to ensure imports to files 
+      // in the package folder can be done. For example import react/jsx-runtime
+      // fails when the resolution using just require.resolve("react") will return 
+      // ...react/index.js which is not the correct path. for files inside the package 
+      // like react/jsx-runtime which then resolves to ...react/index.js/jsx-runtime. 
+      // Remember this will only work for packages that have and index.js file in the root.
+      // react:require.resolve("react"),
+      react: dirname(require.resolve("react")),
+      // "react/jsx-runtime": `${dirname(require.resolve("react"))}/jsx-runtime`,
       // Dependencies below are peerDependencies of @storybook/nextjs
       // Had to lift them up so that they are resolved correctly.
       "postcss-loader": require.resolve('postcss-loader'),
